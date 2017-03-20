08404b203b42dfcd706acf8f3616fd271efecbb8
Keren Megory-Cohen <keren.megorycohen@colorado.edu>
2017-03-20 00:35:59 -0600

    Submit Lab 4

---
+++ Lab 4/STD/STD/DetailViewController.swift
@@ -0,0 +1,65 @@
+//
+//  DetailViewController.swift
+//  STD
+//
+//  Created by Keren Megory-Cohen on 3/19/17.
+//  Copyright © 2017 Keren Megory-Cohen. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController, UIWebViewDelegate {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var webSpinner: UIActivityIndicatorView!
+    
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+    
+    //UIWebViewDelegate method that is called when a web page begins to load
+    func webViewDidStartLoad(_ webView: UIWebView) { webSpinner.startAnimating()
+    }
+    
+    //UIWebViewDelegate method that is called when a web page loads successfully
+    func webViewDidFinishLoad(_ webView: UIWebView) { webSpinner.stopAnimating()
+    }
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.description
+                loadWebPage(detail.description)
+            }
+        }
+    }
+    
+    func loadWebPage(_ urlString: String){
+        //the urlString should be a propery formed url //creates a NSURL object
+        let url = URL(string: urlString)
+        //create a NSURLRequest object
+        let request = URLRequest(url: url!)
+        //load the NSURLRequest object in our web view
+        webView.loadRequest(request)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Lab 4/STD/STD/MasterViewController.swift
@@ -0,0 +1,121 @@
+//
+//  MasterViewController.swift
+//  STD
+//
+//  Created by Keren Megory-Cohen on 3/19/17.
+//  Copyright © 2017 Keren Megory-Cohen. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+
+    var detailViewController: DetailViewController? = nil
+    var objects = [Any]()
+    
+    var types = [[String : String]]()
+    
+    func getDataFile() -> String? {
+        //use a Bundle object of the directory for our application to retrieve the pathname of artistalbums.plist
+        guard let pathString = Bundle.main.path(forResource: "stds", ofType: "plist") else {
+            return nil
+        }
+        return pathString
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //self.navigationItem.leftBarButtonItem = self.editButtonItem
+        //let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
+        //self.navigationItem.rightBarButtonItem = addButton
+        
+        guard let path = getDataFile() else{
+            print("Error loading file")
+            return
+        }
+        
+        //load the data of the plist file into the dictionary
+        let alldata = NSDictionary(contentsOfFile: path) as! [String: [[String : String]]]
+        print(alldata)
+        if alldata.isEmpty != true {
+            types = Array(alldata["types"]!)
+        }
+        
+        if let split = self.splitViewController {
+            let controllers = split.viewControllers
+            self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+        
+        }
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        self.clearsSelectionOnViewWillAppear = self.splitViewController!.isCollapsed
+        super.viewWillAppear(animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*func insertNewObject(_ sender: Any) {
+        objects.insert(NSDate(), at: 0)
+        let indexPath = IndexPath(row: 0, section: 0)
+        self.tableView.insertRows(at: [indexPath], with: .automatic)
+    }*/
+
+    // MARK: - Segues
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                //assign type to dictionary at specified index
+                let type = types[indexPath.row]
+                //get name from dictionary
+                let name = type["name"]! as String
+                //get url from dictionary
+                let url = type["url"]! as String
+                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+                controller.detailItem = url as AnyObject?
+                controller.title = name
+                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+
+    // MARK: - Table View
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return types.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let character = types[indexPath.row]
+        cell.textLabel!.text = character["name"]! as String
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return false
+    }
+
+    /*override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            objects.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        }
+    }*/
+
+
+}
+
