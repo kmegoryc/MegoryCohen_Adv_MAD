06b0271ba6b5d64222d788a1ad07f605af61817f
Keren Megory-Cohen <keren.megorycohen@colorado.edu>
2017-04-14 11:46:39 -0600

    Project 2: Milestone 2

---
+++ Project 2/m2/Ember/app/src/main/java/edu/colorado/keren_megorycohen/ember/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.colorado.keren_megorycohen.ember;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
